32cad85f9f84366dc18873fe4fad9acf9b969e69
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-04-18 18:03:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 function happyHolidays() {
- return "Happy holidays!"
+ return ("happy Holidays")
 }
 function happyHolidaysTo(name) {
-  return "Happy holidays, you!"
+  return "Happy holidays,  "
 }
 function happyHolidayTo(holiday,name) {
   return "Happy Independence Day, you!"
